e07a4f8e04572e63556d27fb889f260051754f2c
Titouan BENOIT <nightbringer4ever@gmail.com>
2017-07-12 13:23:26 +0200

    work on resources

--- src/game-objects/resources/resource.ts
+++ src/game-objects/resources/resource.ts
@@ -2,11 +2,44 @@
 abstract class Resource {
 
     protected name: String;
+    protected total: number;
+    protected max: number;
+    protected min: number;
     protected icon: any;
 
 
     public getName(): String {
         return this.name;
     }
+
+    public getMax(): number {
+        return this.max;
+    }
+
+    public getMin(): number {
+        return this.min;
+    }
+
+    public sub(number: number)
+    {
+        return this.total - number;
+    }
+
+    public add(number: number)
+    {
+        return this.total + number;
+    }
+
+    public isMaxed(): boolean
+    {
+        return (this.total >= this.max);
+    }
+
+    public isMined(): boolean
+    {
+        return (this.total <= this.min);
+    }
+
+
 
 }
