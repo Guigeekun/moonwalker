37e3ae4a35441379727abba6ef2cea43f447e039
Guigeek <geegeek21@gmail.com>
2017-07-13 12:03:43 +0200

    building in progress

--- src/game-objects/buildings/building.ts
+++ src/game-objects/buildings/building.ts
@@ -2,8 +2,33 @@
 abstract class Building {
 
     protected name: String;
+    protected icon: any;
     protected buyingDescription: String;
     protected mainDescription: String;
+    protected level: number;
 
-    // Buy 
+    //
+    protected buildingTime: Object;
+    protected buildingStarted: Date;
+
+    // Buy
+    protected cost: {titanium:{a:number,b:number},rareEarths:{a:number,b:number},helium:{a:number,b:number},energy:{a:number}};
+
+ public getLvl(): number {
+        return this.level;
+    }
+ public getNextLvl(): number {
+        return this.level+1;
+    }
+public getCost(): Object{
+        return this.cost;
+}
+public canBeBuilt(): boolean{
+    if (this.getLvl = 10){
+        return false
+    }else{
+        return true
+    }
+
+}
 }
