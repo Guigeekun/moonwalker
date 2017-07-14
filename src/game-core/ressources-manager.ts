c542ac46e5dc3b71f55e053fbbbb69bc2e007fbf
Guigeek <geegeek21@gmail.com>
2017-07-14 12:44:32 +0200

    buildings cost should work

--- src/game-core/ressources-manager.ts
+++ src/game-core/ressources-manager.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs/Observable";
 import { Titanium } from '../game-objects/resources/titanium';
 import { Helium } from '../game-objects/resources/helium';
 import { RareEarths } from '../game-objects/resources/rare-earths';
+//import { Building } from '../game-objects/buildings/building';
 
 @Injectable()
 export class ResourcesManager {
@@ -35,13 +36,27 @@ export class ResourcesManager {
     }
 
     public verifyCost(building: any){
-        //todo
-        return {titanium: true, rareEarths:true, helium: true, energy: true};
+        let verif = {titanium:false,rareEarths:false,helium:false,energy:false};
+        if(this.currentTitanium-building.titaniumCost >= 0){
+            verif.titanium = true;
+        }
+        if(this.currentRareEarths-building.rareEarthsCost >= 0){
+            verif.rareEarths = true;
+        }
+        if(this.currentHelium-building.heliumCost >= 0){
+            verif.helium = true;
+        }
+        if(this.currentEnergy-building.energyCost >= 0){
+            verif.energy = true;
+        }
+        return verif
     }
 
     public buyBuildings(building: any){
-
-        this.store.dispatch({type:'RAREEARTHS_SUB', payload: 200});
-
+        this.store.dispatch({type:'TITANIUM_SUB', payload: building.titaniumCost});
+        this.store.dispatch({type:'RAREEARTHS_SUB', payload: building.rareEarthsCost});
+        this.store.dispatch({type:'HELIUM_SUB', payload: building.heliumCost});
+        this.store.dispatch({type:'ENERGY_SUB', payload: building.energyCost});
+        return 1
     }
 }
